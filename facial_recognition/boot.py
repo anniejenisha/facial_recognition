789e31035b76d58112bdba5880be48686eb46751
Annie Jenisha <merlinepeter12@gmail.com>
2026-07-24 09:19:12 +0000

    Boot has been deleted

--- facial_recognition/boot.py
+++
@@ -1,4 +0,0 @@
-import frappe
-
-def boot_session(bootinfo):
-    bootinfo.default_route = "/app/facial_recognition"
--- facial_recognition/hooks.py
+++ facial_recognition/hooks.py
@@ -34,7 +34,6 @@ app_license = "mit"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "facial_recognition/public/scss/website"
-boot_session = "facial_recognition.boot.boot_session"
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
 # webform_include_css = {"doctype": "public/css/doctype.css"}
